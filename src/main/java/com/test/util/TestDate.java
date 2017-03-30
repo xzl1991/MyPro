b875041f702bb25a4ed7b9bb39418632eedc95a9
xuzelong <xuzelong@gomeholdings.com>
2017-03-30 13:50:06 +0800

    lang.date

---
+++ src/main/java/com/test/util/TestDate.java
@@ -0,0 +1,23 @@
+package com.test.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.sunline.ark.support.utils.DateUtils;
+
+public class TestDate {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		System.out.println(new Date());
+//		System.out.println(new Date("20161001"));
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyymmdd");
+	Date date = sdf.parse("20171001");
+	Date date1 = sdf.parse("20171001");
+	 int a = 	org.apache.commons.lang.time.DateUtils.truncatedCompareTo(date,date1, Calendar.DATE);
+	 System.out.println(a);
+	}
+
+}
